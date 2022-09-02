7bbcd6e2c0771b53f2ea721760d429b15d30f345
savannah8 <willengitonga.wg@gmail.com>
2022-09-02 10:09:46 +0300

    added checking whitelist js function and fronted html

--- whitelist-app/pages/index.js
+++ whitelist-app/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   // loading = true if waiting for transaction to be mined
   const [loading, setLoading] = useState(false);
 
-  const [numberOfWhiteListed, setNumberOfWhitelisted] = useState(0);
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
 
   //reference for the web3 modal
   const Web3ModalRef = useRef();
@@ -95,5 +95,110 @@ const getNumberOfWhitelisted = async () => {
 
 
 /** checkIfAddressInWhitelist: checks if the address is in the whitelist**/
-const checkIfAddressInWhitelist
-}
+const checkIfAddressInWhitelist = async () => {
+  try{
+    const signer = await getProviderOrSigner(true);
+    const whitelistContract = new Contract(
+      WHITELIST_CONTRACT_ADDRESS,
+      abi,
+      signer
+    );
+    //get address connected to the metamask
+    const address = await signer.getAddress();
+    //call whitelistedAddresses from the contract 
+    const _joinedWhitelsit = await whitelistContract.whitelistedAddresses(
+      address
+    );
+    setJoinedWhiteList(_joinedWhitelsit);
+  }
+  catch(err){
+    console.error(err);
+  }
+};
+
+const connectWallet = async () => {
+  try {
+    // Get the provider from web3Modal, which in our case is MetaMask
+    // When used for the first time, it prompts the user to connect their wallet
+    await getProviderOrSigner();
+    setWalletConnected(true);
+
+    checkIfAddressInWhitelist();
+    getNumberOfWhitelisted();
+  } catch (err) {
+    console.error(err);
+  }
+};
+  /*
+    renderButton: Returns a button based on the state of the dapp
+  */
+  const renderButton = () => {
+      if (walletConnected) {
+        if (joinedWhitelist) {
+          return (
+            <div className={styles.description}>
+              Thanks for joining the Whitelist!
+            </div>
+          );
+        } else if (loading) {
+          return <button className={styles.button}>Loading...</button>;
+        } else {
+          return (
+            <button onClick={addAddressToWhitelist} className={styles.button}>
+              Join the Whitelist
+            </button>
+          );
+        }
+      } else {
+        return (
+          <button onClick={connectWallet} className={styles.button}>
+            Connect your wallet
+          </button>
+        );
+      }
+    };
+  
+    useEffect(() => {
+      // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+      if (!walletConnected) {
+        // Assign the Web3Modal class to the reference object by setting it's `current` value
+        // The `current` value is persisted throughout as long as this page is open
+        web3ModalRef.current = new Web3Modal({
+          network: "rinkeby",
+          providerOptions: {},
+          disableInjectedProvider: false,
+        });
+        connectWallet();
+      }
+    }, [walletConnected]);
+
+    return (
+      <div>
+        <Head>
+          <title>Whitelist Dapp</title>
+          <meta name="description" content="Whitelist-Dapp" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className={styles.main}>
+          <div>
+            <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+            <div className={styles.description}>
+              Its an NFT collection for developers in Crypto.
+            </div>
+            <div className={styles.description}>
+              {numberOfWhitelisted} have already joined the Whitelist
+            </div>
+            {renderButton()}
+          </div>
+          <div>
+            <img className={styles.image} src="./crypto-devs.svg" />
+          </div>
+        </div>
+  
+        <footer className={styles.footer}>
+          Made with &#10084; by Crypto Devs
+        </footer>
+      </div>
+    );
+  }
+
